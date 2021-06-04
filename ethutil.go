91a0ac72e7dd214e652806e6f1a127a1862aaac7
warrior21st <123767168@qq.com>
2021-06-04 16:02:52 +0800

    Update ethutil.go

--- ethutil.go
+++ ethutil.go
@@ -50,7 +50,8 @@ func EcRecover(digestHash []byte, sig []byte) (common.Address, error) {
 		return common.Address{}, errors.New("invalid Ethereum signature (V is not 27 or 28)")
 	}
 	//sig[64] -= 27 // Transform yellow paper V from 27/28 to 0/1
-	sig2 := sig
+	sig2 := make([]byte, len(sig))
+	copy(sig, sig2)
 	sig2[64] -= 27
 	pubKey, err := crypto.SigToPub(digestHash, sig2)
 	if err != nil {
